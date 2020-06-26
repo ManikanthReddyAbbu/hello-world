4d27b20056d42ef1b9384ab9e619d56dd710a370
ManikanthReddy <reddymanikanth8@gmail.com>
2020-06-27 00:29:53 +0530

    Jenkinsfile commit

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,27 @@
+pipeline{
+    agent any{
+        stages {
+        stage('compile') {
+            steps {
+                withMaven(maven : 'Maven')
+                sh 'mvn clean compile'
+            }
+        }
+
+        stage('Test') {
+            steps {
+                withMaven(maven : 'Maven')
+                sh 'mvn test'
+            }
+        }
+
+        stage('deploy') {
+            steps {
+                withMaven(maven : 'Maven')
+                sh 'mvn deploy'
+            }
+        }
+
+        }
+    }
+}
